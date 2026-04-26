4f8112a328f905c1eec1531b76c2d1d1e53a32d9
nathanvlz <163420410+nathanvlz@users.noreply.github.com>
2026-04-25 21:42:59 -0300

    Add user validation function

--- src/validation/userValidation.js
+++ src/validation/userValidation.js
@@ -1,1 +1,6 @@
+function validateUser(user) {
+  if (!user.name) throw new Error("Nome obrigatório");
+  if (!user.email) throw new Error("Email obrigatório");
+}
 
+module.exports = { validateUser };
